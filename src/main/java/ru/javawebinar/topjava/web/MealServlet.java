4c27b4541ba8a3dc44f988dcd47aa0eced9eb8c3
Ivan Potapenko <dreamer4ik@gmail.com>
2021-02-07 21:15:44 +0200

    done 1.2.5

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -1,6 +1,7 @@
 package ru.javawebinar.topjava.web;
 
 import org.slf4j.Logger;
+import ru.javawebinar.topjava.util.MealsUtil;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -8,8 +9,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.time.LocalTime;
 
 import static org.slf4j.LoggerFactory.getLogger;
+import static ru.javawebinar.topjava.util.MealsUtil.MEAL_LIST;
 
 public class MealServlet extends HttpServlet {
 
@@ -19,9 +22,12 @@ public class MealServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         log.debug("redirect to meals");
 
-//        request.getRequestDispatcher("/meals.jsp").forward(request, response);
-        response.sendRedirect("meals.jsp");
+        request.setAttribute("meals", MealsUtil.filteredByStreams(MEAL_LIST , LocalTime.MIN, LocalTime.MAX, 2000));
+        request.getRequestDispatcher("/meals.jsp").forward(request, response);
+       // response.sendRedirect("meals.jsp");
     }
+
+
 }
 
 
--- src/main/webapp/meals.jsp
+++ src/main/webapp/meals.jsp
@@ -2,11 +2,37 @@
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <html lang="ru">
 <head>
+    <style>
+        .normal {
+            color: green;
+        }
+        .excess {
+            color: red;
+        }
+    </style>
     <title>Meals</title>
 </head>
 <body>
-<h3><a href="index.html">Home</a></h3>
-<hr>
-<h2>Meals</h2>
+<section>
+    <h3><a href="index.html">Home</a></h3>
+    <hr>
+    <h2>Meals</h2>
+    <table border="1" cellpadding="8" cellspacing="0">
+
+        <tr>
+            <th>Date</th>
+            <th>Description</th>
+            <th>Calories</th>
+        </tr>
+        <c:forEach items="${meals}" var="meal">
+            <jsp:useBean id="meal" type="ru.javawebinar.topjava.model.MealTo"/>
+            <tr class="${meal.excess ? "excess" : "normal"}">
+                <td>${meal.formattedDateTime}</td>
+                <td>${meal.description}</td>
+                <td>${meal.calories}</td>
+            </tr>
+        </c:forEach>
+    </table>
+</section>
 </body>
 </html>
