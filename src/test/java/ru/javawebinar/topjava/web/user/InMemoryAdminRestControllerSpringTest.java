f9cb4493d546aaf5ec08b7071a6db74e868b77e7
Ivan Potapenko <dreamer4ik@gmail.com>
2021-02-24 18:30:48 +0200

    failed  InMemoryAdminRestControllerSpringTest & InMemoryAdminRestControllerTest

--- src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
+++ src/test/java/ru/javawebinar/topjava/web/user/InMemoryAdminRestControllerSpringTest.java
@@ -3,11 +3,14 @@ package ru.javawebinar.topjava.web.user;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
 import ru.javawebinar.topjava.repository.inmemory.InMemoryUserRepository;
 import ru.javawebinar.topjava.service.UserServiceTest;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
@@ -16,6 +19,10 @@ import static ru.javawebinar.topjava.UserTestData.NOT_FOUND;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
 
+@ContextConfiguration({
+        "classpath:spring/springtest-app.xml"
+})
+@RunWith(SpringRunner.class)
 public class InMemoryAdminRestControllerSpringTest {
     private static final Logger log = LoggerFactory.getLogger(InMemoryAdminRestControllerTest.class);
 
