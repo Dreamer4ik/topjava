cce4ecf6b177284a47f2f02ead740b1a00c06c97
Ivan Potapenko <dreamer4ik@gmail.com>
2021-02-25 19:49:18 +0200

    + pom xml dependency + fix user

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -4,9 +4,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import org.hibernate.validator.constraints.Range;
 import org.springframework.util.CollectionUtils;
-
+import org.hibernate.validator.constraints.Range;
 import javax.persistence.*;
 import java.util.Collection;
 import java.util.Date;
